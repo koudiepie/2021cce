f040c49620f5770213d2120fdfcf0aaa6166c1e0
koudiepie <91250563+koudiepie@users.noreply.github.com>
2021-10-07 17:39:30 +0800

    Add files via upload

---
+++ 01-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a=123, b=10;
+    float  c=3.14156265358979;
+    double d=3.14156265358979;
+    printf("%f\n", c );
+    printf("%f\n", d );
+    Printf("%.4f\n", d );
+    printf("%.9f\n", d );
+    printf("%.12f\n", d );
+}
